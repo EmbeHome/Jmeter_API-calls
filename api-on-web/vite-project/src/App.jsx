e516470a922e730bbc49206915efd7a3db7dd8d6
HarshaSri-Sameera <harshasrisameera2002@gmail.com>
2023-02-06 15:47:42 +0530

    new commit

---
+++ api-on-web/vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+function App() {
+  const [fact, setFact] = useState("");
+
+  const catFactGenerator = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setFact(res.data.fact)
+    });
+  };
+
+  useEffect(() => {
+    catFactGenerator();
+  },[]);
+
+  return (  
+    <div className="App">
+      <button onClick={catFactGenerator}>Generate a cat fact</button>
+      <p>{fact}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,31 @@
+// Represents the api we are building
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json()); // middleware
+
+app.listen(
+    PORT,
+    () => console.log(`It's alive on http://localhost:${PORT}`)
+)
+
+app.get('/data', (req, res) => {
+    res.status(200).send({ 
+        name: 'John',
+        age: '20'
+    });
+});
+
+app.post('/data/:id', (req, res) => {
+    const { id } = req.params;
+    const { surname } = req.body;
+
+    if(!surname) {
+        res.status(404).send({ message: "No surname found!" })
+    }
+
+    res.send({
+        name: `John with your ${surname} and ID of ${id}`,
+    })
+});
